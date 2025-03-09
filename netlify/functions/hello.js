8f2a081212df505ce5663ac86acd489d27e903bc
Raphael Ferrand <goldenraphti@users.noreply.github.com>
2025-03-09 20:49:43 +0100

    test 4

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,8 +1,15 @@
 exports.handler = async (event) => {
   const subject =
     event.queryStringParameters.name || "World loooser - you failed";
+
+  const response = JSON.stringify({ subject: subject });
+
   return {
     statusCode: 200,
-    body: `Hello ${subject}!`,
+    body: response,
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+      "Access-Control-Allow-Origin": "*",
+    },
   };
 };
